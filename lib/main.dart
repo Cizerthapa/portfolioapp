729712e8d0120aa9ec0ed587032e0bcd0eba58e7
Cizer Thapa <cizerthapa@gmail.com>
2025-12-03 17:34:14 +0545

    chore: firebase setup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:portfolio_web/firebase_options.dart';
 import 'package:portfolio_web/screens/home_screen.dart';
 import 'package:portfolio_web/utils/responsive_wrapper.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const PortfolioApp());
 }
 
