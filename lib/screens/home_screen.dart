c708e36e9bad538bcb278322d360a59ddbbb9cf0
Cizer Thapa <cizerthapa@gmail.com>
2025-12-24 16:47:02 +0545

    chore: removed the padding container to get the full width

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:portfolio_web/widgets/navbar.dart';
 import 'package:portfolio_web/widgets/hero_section.dart';
 import 'package:portfolio_web/widgets/about_section.dart';
@@ -6,10 +7,12 @@ import 'package:portfolio_web/widgets/skills_section.dart';
 import 'package:portfolio_web/widgets/projects_section.dart';
 import 'package:portfolio_web/widgets/contact_section.dart';
 import 'package:portfolio_web/widgets/footer.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import 'package:portfolio_web/utils/responsive_mixin.dart';
 
 import 'package:portfolio_web/widgets/dedication_banner.dart';
+import 'package:portfolio_web/widgets/experience_section.dart';
+import 'package:portfolio_web/widgets/services_section.dart';
 
 class HomeScreen extends StatelessWidget with ResponsiveMixin {
   const HomeScreen({super.key});
@@ -24,28 +27,22 @@ class HomeScreen extends StatelessWidget with ResponsiveMixin {
             const DedicationBanner(),
             const Navbar(),
             const HeroSection(),
-            Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: responsiveValue<double>(
-                  context,
-                  mobile: 20.w,
-                  tablet: 40.w,
-                  desktop: 80.w,
-                ),
-              ),
-              child: Column(
-                children: [
-                  SizedBox(height: spacingXXL),
-                  const AboutSection(),
-                  SizedBox(height: spacingXXL),
-                  const SkillsSection(),
-                  SizedBox(height: spacingXXL),
-                  const ProjectsSection(),
-                  SizedBox(height: spacingXXL),
-                  const ContactSection(),
-                  SizedBox(height: spacingXXL),
-                ],
-              ),
+            Column(
+              children: [
+                spacingXXL.toInt().verticalSpace,
+                const AboutSection(),
+                spacingXXL.toInt().verticalSpace,
+                const ServicesSection(),
+                spacingXXL.toInt().verticalSpace,
+                const ExperienceSection(),
+                spacingXXL.toInt().verticalSpace,
+                const SkillsSection(),
+                spacingXXL.toInt().verticalSpace,
+                const ProjectsSection(),
+                spacingXXL.toInt().verticalSpace,
+                const ContactSection(),
+                spacingXXL.toInt().verticalSpace,
+              ],
             ),
             const Footer(),
           ],
