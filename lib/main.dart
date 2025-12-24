e38ee10d8eceb30d0a2ab107b0163948b09212d7
Cizer Thapa <cizerthapa@gmail.com>
2025-12-24 12:52:41 +0545

    chore: added the work of new screen in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:portfolio_web/firebase_options.dart';
+import 'package:portfolio_web/screens/about_screen.dart';
+import 'package:portfolio_web/screens/contact_screen.dart';
 import 'package:portfolio_web/screens/home_screen.dart';
 import 'package:portfolio_web/utils/responsive_wrapper.dart';
 
@@ -23,10 +25,21 @@ class PortfolioApp extends StatelessWidget {
           primarySwatch: Colors.blue,
           fontFamily: 'Inter',
           useMaterial3: true,
+          scaffoldBackgroundColor: Colors.white,
+          canvasColor: Colors.white,
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.blue,
+            surface: Colors.white,
+          ),
         ),
-        darkTheme: ThemeData.dark().copyWith(useMaterial3: true),
+        darkTheme: ThemeData.dark(useMaterial3: true),
         themeMode: ThemeMode.light,
-        home: const HomeScreen(),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const HomeScreen(),
+          '/about': (context) => const AboutScreen(),
+          '/contact': (context) => const ContactScreen(),
+        },
       ),
     );
   }
